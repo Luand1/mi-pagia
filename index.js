fa214820dbcc5a1f95f8d193e2ee693a715211a1
luand1 <luand.suramericana@gmail.com>
2025-01-19 17:16:39 -0500

    mejora

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send("hola, mundo");
 })
 
